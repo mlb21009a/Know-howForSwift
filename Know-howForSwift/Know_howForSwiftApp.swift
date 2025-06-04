36acd89a3c7d4eec26c40f71ac6b41f3021673dc
mlb21009a <mlb21009a@gmail.com>
2025-06-04 14:30:47 +0900

    Add AppDelegate

--- Know-howForSwift/Know_howForSwiftApp.swift
+++ Know-howForSwift/Know_howForSwiftApp.swift
@@ -9,6 +9,8 @@ import SwiftUI
 
 @main
 struct Know_howForSwiftApp: App {
+    @UIApplicationDelegateAdaptor(AppDelegate.self) var appDelegate
+
     var body: some Scene {
         WindowGroup {
             NavigationStack {
@@ -41,4 +43,12 @@ struct Know_howForSwiftApp: App {
 
         }
     }
+}
+
+import UIKit
+class AppDelegate: UIResponder, UIApplicationDelegate {
+    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey : Any]? = nil) -> Bool {
+        return true
+    }
+    // 必要に応じて処理を追加
 }
